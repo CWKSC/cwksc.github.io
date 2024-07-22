16262b63f2648449caf30a382e2a83d81e5c3e0e
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-07-22 11:41:56 +0800

    Add about me in root page

--- volume/src/pages/index.tsx
+++ volume/src/pages/index.tsx
@@ -8,6 +8,31 @@ export default function Home(): JSX.Element {
             title={`${siteConfig.title}`}
             description="description">
 
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: '32px',
+                    gap: '16px',
+                }}>
+
+                <img style={{
+                    verticalAlign: 'middle',
+                    width: '128px',
+                    height: '128px',
+                    borderRadius: '50%'
+                }} src="./img/icon.jpg" alt="Avatar" />
+
+                <h1> About me 關於我 </h1>
+
+                <p> 一個什麼都學，但什麼都不精通的人 </p>
+
+                <p> 畢業於 City University of Hong Kong, Computer Science </p>
+
+            </div>
+
         </Layout>
     );
 }
